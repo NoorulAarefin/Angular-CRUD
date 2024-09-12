84dd0c77a5ed740a08fe254b5ad833470cad9a09
NoorulAarefin <107918965+NoorulAarefin@users.noreply.github.com>
2024-09-12 18:51:00 +0530

    Employee form and Data table and logic

---
+++ Angular-CRUD/src/app/Models/Employee.ts
@@ -0,0 +1,12 @@
+export class EmployeeModel {
+
+  empId : number =0;
+  empName : string ='';
+  empContact : string='';
+  empAddress : string ='';
+  empCity : string ='';
+  empPinCode : number =0;
+  empSalary : number=0;
+  empEmailId: string ='';
+
+}
--- Angular-CRUD/src/app/app.component.ts
+++ Angular-CRUD/src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { EmployeeModel } from './Models/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-CRUD';
+  empForm: FormGroup = new FormGroup({});
+
+  Employee :  EmployeeModel = new EmployeeModel();
+  EmpList : EmployeeModel[]=[];
+  constructor() {
+    this.empForm;
+  }
+
+
+
+  newEmpForm (){
+    this.empForm = new FormGroup ({
+
+      empId : new FormControl(this.Employee.empId),
+      empName : new FormControl(this.Employee.empName),
+      empEmailId : new FormControl(this.Employee.empEmailId),
+      empContact : new FormControl(this.Employee.empContact),
+      empAddress : new FormControl(this.Employee.empAddress),
+      empPinCode : new FormControl(this.Employee.empPinCode),
+      empSalary : new FormControl(this.Employee.empSalary),
+      empCity : new FormControl(this.Employee.empCity)
+    });
+  }
+
+  onSave (){
+    const oldData = localStorage.getItem("EmpData");
+    if(oldData != null){
+      const parseData = JSON.parse(oldData);
+      this.empForm.controls['empId'].setValue(parseData.length +1);
+    }
+  }
+
+
 }
